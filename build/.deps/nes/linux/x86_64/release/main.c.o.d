7caf38425476ce5a23baa67b602bb1ec0c44aea6
Roman <rpilyushin@gmail.com>
2024-08-31 17:53:51 +0300

    First build xmake

---
+++ build/.deps/nes/linux/x86_64/release/main.c.o.d
@@ -0,0 +1,24 @@
+{
+    files = {
+        "main.c"
+    },
+    values = {
+        "/usr/bin/gcc",
+        {
+            "-m64",
+            "-fvisibility=hidden",
+            "-Wall",
+            "-Wextra",
+            "-O3",
+            "-std=c11",
+            "-Iinc",
+            "-Iport",
+            "-D_REENTRANT",
+            "-isystem",
+            "/usr/include/SDL2",
+            "-DNDEBUG"
+        }
+    },
+    depfiles_gcc = "main.o: main.c inc/nes.h port/nes_port.h port/nes_conf.h inc/nes_rom.h  inc/nes_cpu.h inc/nes_ppu.h port/nes_conf.h inc/nes_apu.h  inc/nes_mapper.h\
+"
+}
---
+++ build/.deps/nes/linux/x86_64/release/nes.d
@@ -0,0 +1,27 @@
+{
+    files = {
+        "build/.objs/nes/linux/x86_64/release/src/nes.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_apu.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_cpu.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper0.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper117.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper180.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper2.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper3.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper7.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper/nes_mapper94.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_mapper.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_ppu.c.o",
+        "build/.objs/nes/linux/x86_64/release/src/nes_rom.c.o",
+        "build/.objs/nes/linux/x86_64/release/port/nes_port.c.o",
+        "build/.objs/nes/linux/x86_64/release/main.c.o"
+    },
+    values = {
+        "/usr/bin/g++",
+        {
+            "-m64",
+            "-s",
+            "-lSDL2"
+        }
+    }
+}
